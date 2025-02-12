a35947d7be157228c616f7281391f0b91cfaf4d5
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-12 12:40:49 -0300

    [wip] Fixed layout component name

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,4 +1,4 @@
-export default function Headerr() {
+export default function Header() {
   return (
     <header className="flex items-center justify-center w-full h-12 bg-gray-50 shadow-md">
       <h1 className="text-green-500 font-bold">SWSarter</h1>
