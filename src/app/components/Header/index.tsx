137bb868be344c656ad55fb9b3ac00d7bffddff2
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-12 12:40:30 -0300

    [wip] Created layout component Header

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export default function Headerr() {
+  return (
+    <header className="flex items-center justify-center w-full h-12 bg-gray-50 shadow-md">
+      <h1 className="text-green-500 font-bold">SWSarter</h1>
+    </header>
+  );
+}
