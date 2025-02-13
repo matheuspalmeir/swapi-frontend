3ce88ce7fe4f0f92e8d6fc1e421ea19a772db429
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-13 12:25:16 -0300

    [fix] Updated requests API Base URL

--- src/app/util/requests.ts
+++ src/app/util/requests.ts
@@ -1,7 +1,7 @@
 export const SWAPI = {
-  BASE_URL: "http://localhost:8000/api/",
-  GET_PEOPLE: "http://localhost:8000/api/people/",
-  GET_PEOPLE_BY_NAME: "http://localhost:8000/api/people?search=",
-  GET_MOVIES: "http://localhost:8000/api/films/",
-  GET_MOVIES_BY_TITLE: "http://localhost:8000/api/films?search=",
+  BASE_URL: "http://swapi-gateway:8000/api/",
+  GET_PEOPLE: "http://swapi-gateway:8000/api/people/",
+  GET_PEOPLE_BY_NAME: "http://swapi-gateway:8000/api/people?search=",
+  GET_MOVIES: "http://swapi-gateway:8000/api/films/",
+  GET_MOVIES_BY_TITLE: "http://swapi-gateway:8000/api/films?search=",
 };
