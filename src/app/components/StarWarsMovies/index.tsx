4768b9350be4a2c5d28e48579a034990fb0b1482
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-12 23:32:15 -0300

    [wip] Created the movie and people component (list item)

---
+++ src/app/components/StarWarsMovies/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Button from "../Button";
+import { TStarWarsMovie, TStarWarsMovies } from "@/app/types";
+
+type StarWarsMoviesProps = {
+  data?: TStarWarsMovies;
+  onClickDetails: (person: TStarWarsMovie) => void;
+};
+
+export default function StartWarsMovies({
+  onClickDetails,
+  data,
+}: StarWarsMoviesProps) {
+  console.log("StarWarsMovies Props:", { data, onClickDetails });
+  return (
+    <>
+      {data?.map((movie, index) => (
+        <div
+          key={`${movie.title}-${index}`}
+          className="flex flex-row flex-1 flex-nowrap justify-between border-b-2 border-gray-300 p-2"
+        >
+          <h1 className="font-bold">{movie.title}</h1>
+          <Button onClick={() => onClickDetails(movie)} className="w-40">
+            See Details
+          </Button>
+        </div>
+      ))}
+    </>
+  );
+}
---
+++ src/app/components/StarWarsPeople/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Button from "../Button";
+import { TStarWarsPeople, TStarWarsPerson } from "@/app/types";
+
+type StarWarsPeopleProps = {
+  data?: TStarWarsPeople;
+  onClickDetails: (person: TStarWarsPerson) => void;
+};
+
+export default function StartWarsPeople({
+  data,
+  onClickDetails,
+}: StarWarsPeopleProps) {
+  console.log("StarWarsPeople Props:", { data, onClickDetails });
+  return (
+    <>
+      {data?.map((person, index) => (
+        <div
+          key={`${person.name}-${index}`}
+          className="flex flex-row flex-1 flex-nowrap justify-between border-b-2 border-gray-300 p-2"
+        >
+          <h1 className="font-bold">{person.name}</h1>
+          <Button onClick={() => onClickDetails(person)} className="w-40">
+            See Details
+          </Button>
+        </div>
+      ))}
+    </>
+  );
+}
