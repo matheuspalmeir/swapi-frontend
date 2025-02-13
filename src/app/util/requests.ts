aa1d477ff528a0dd166eaa108b72a33d7f1aca93
Matheus Palmeira <matheus_160497@hotmail.com>
2025-02-12 23:30:26 -0300

    [wip] Added the SWAPI constant with API urls

---
+++ src/app/util/requests.ts
@@ -0,0 +1,7 @@
+export const SWAPI = {
+  BASE_URL: "https://swapi.dev/api/",
+  GET_PEOPLE: "https://swapi.dev/api/people/",
+  GET_PEOPLE_BY_NAME: "https://swapi.dev/api/people/?search=",
+  GET_MOVIES: "https://swapi.dev/api/films/",
+  GET_MOVIES_BY_TITLE: "https://swapi.dev/api/films/?search=",
+};
